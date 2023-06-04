38b72ea8e27d291e0171d8384f9481a519e44ff1
Aditya kumar <adi54321kumar@gmail.com>
2023-06-04 22:49:13 +0530

    Nvaigation drawer added

---
+++ app/src/main/java/com/example/googledeveloperscommunityvisualisationtool/Fragments/UpcomingEvents/Adapter.kt
@@ -0,0 +1,44 @@
+package com.example.googledeveloperscommunityvisualisationtool.Fragments.UpcomingEvents
+
+import android.media.Image
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AbsListView.RecyclerListener
+import androidx.recyclerview.widget.RecyclerView
+import com.example.googledeveloperscommunityvisualisationtool.DataClass.Volley.Result
+import com.example.googledeveloperscommunityvisualisationtool.R
+import com.example.googledeveloperscommunityvisualisationtool.databinding.EventsitemlistBinding
+
+class Adapter(private val eventList:List<Result>):RecyclerView.Adapter<Adapter.MyViewHolder>() {
+    class MyViewHolder(val binding: EventsitemlistBinding) :RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding=EventsitemlistBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return MyViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return eventList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        with(holder){
+            with(eventList[position]){
+                binding.eventTitle.text=this.title.toString()
+                binding.GDGname.text=this.chapter?.title.toString()
+                binding.city.text=this.city.toString()
+                var tag=""
+                for (i in 0 until this.tags!!.size){
+                    tag += this.tags[i]
+                }
+                binding.tags.text=tag.toString()
+                binding.description.text=this.description_short.toString()
+                binding.ViewDetailsButton.setOnClickListener {
+
+                }
+            }
+        }
+    }
+
+}
