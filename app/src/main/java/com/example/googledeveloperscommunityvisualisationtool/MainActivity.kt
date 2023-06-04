6f51d16f129e8b1386c675a34c6f41514c588024
Aditya kumar <adi54321kumar@gmail.com>
2023-06-04 22:49:01 +0530

    Nvaigation drawer added

--- app/src/main/java/com/example/googledeveloperscommunityvisualisationtool/MainActivity.kt
+++ app/src/main/java/com/example/googledeveloperscommunityvisualisationtool/MainActivity.kt
@@ -3,34 +3,39 @@ package com.example.googledeveloperscommunityvisualisationtool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProvider
 import com.example.googledeveloperscommunityvisualisationtool.Repository.GdgScrapingRespository
 import com.example.googledeveloperscommunityvisualisationtool.Repository.UpcomingEventRepository
 import com.example.googledeveloperscommunityvisualisationtool.ViewModel.UpcomingEventViewModel
 import com.example.googledeveloperscommunityvisualisationtool.ViewModel.UpcomingEventViewModelFactory
+import com.example.googledeveloperscommunityvisualisationtool.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    lateinit var textView1:TextView
-    lateinit var upcomingEventViewModel:UpcomingEventViewModel
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        textView1=findViewById(R.id.textView1)
+        setContentView(binding.root)
 
-        val upcomingEventRepository=UpcomingEventRepository(this)
-
-        upcomingEventViewModel=ViewModelProvider(this,UpcomingEventViewModelFactory(upcomingEventRepository)).get(UpcomingEventViewModel::class.java)
-
-        CoroutineScope(Dispatchers.IO).launch{
-            upcomingEventViewModel.getResponseViewModel()
-        }
+        binding=ActivityMainBinding.inflate(layoutInflater)
 
         var gdgScrapingRespository=GdgScrapingRespository()
         CoroutineScope(Dispatchers.IO).launch {
             gdgScrapingRespository.scrape()
         }
+
+        val drawerLayout: DrawerLayout =binding.drawerlayout
+        val navView:NavigationView=binding.navView
+
+//        navView.setNavigationItemSelectedListener {
+//
+//        }
+
+
     }
 }
