5e994c8ca704d6b0845839ac48b9da14cb8df0cb
Aditya kumar <adi54321kumar@gmail.com>
2023-08-11 19:44:15 +0530

    clear database added

---
+++ app/src/main/java/com/example/googledeveloperscommunityvisualisationtool/connection/TimerRun.kt
@@ -0,0 +1,17 @@
+package com.example.googledeveloperscommunityvisualisationtool.connection
+
+import com.example.googledeveloperscommunityvisualisationtool.create.utility.model.ActionBuildCommandUtility
+import java.util.Timer
+import java.util.TimerTask
+
+class TimerRun {
+    val timer= Timer()
+    val task=object :TimerTask(){
+        override fun run() {
+        }
+
+    }
+    fun getDetials(){
+
+    }
+}
